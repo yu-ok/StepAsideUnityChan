757d4c8db20a59616de64d2cede98defff25fb41
yu-ok <71647136+yu-ok@users.noreply.github.com>
2020-10-10 20:53:39 +0900

    Initial commit

---
+++ Assets/Destoryer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destoryer : MonoBehaviour
+{
+    GameObject unityChan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(unityChan.transform.position.z-this.gameObject.transform.position.z > 6)
+                {
+                Destroy (this.gameObject);
+                }
+    }
+}
